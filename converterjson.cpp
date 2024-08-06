e492cab0e6f83d903545709cea3d5aa7331e648f
Sergey Reshetnikov <shaman@simmirra.com>
2024-08-06 09:03:07 +0300

    Добавлен проект в репозиторий

---
+++ converterjson.cpp
@@ -0,0 +1,5 @@
+#include "converterjson.h"
+
+ConverterJSON::ConverterJSON() {}
+
+ConverterJSON::~ConverterJSON() {}
---
+++ converterjson.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class ConverterJSON {
+public:
+  ConverterJSON();
+  ~ConverterJSON();
+};
