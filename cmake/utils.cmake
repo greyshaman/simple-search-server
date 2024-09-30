b2d5451c4038c5e597972d2c3fd5613ef4728d9d
Sergey Reshetnikov <shaman@simmirra.com>
2024-09-30 12:16:47 +0300

    Fixed utils.cmake from incorrect win32 detection and path to script file

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -7,7 +7,7 @@ endmacro()
 
 
 function(win_copy_deps_to_target_dir target)
-    if(NOT_WIN32)
+    if(NOT WIN32)
         return()
     endif()
 
@@ -17,7 +17,7 @@ function(win_copy_deps_to_target_dir target)
         set(has_runtime_dll_genex YES)
 
         add_custom_command(TARGET ${target} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -P "${SearchEngine_SOURCE_DIR}/cmake/silent_copy.cmake"
+            COMMAND ${CMAKE_COMMAND} -P "${search-engine_SOURCE_DIR}/cmake/silent_copy.cmake"
                 "$<TARGET_RUNTIME_DLLS:${target}>" "$<TARGET_FILE_DIR:${target}>"
             COMMAND_EXPAND_LISTS
         )
@@ -29,12 +29,12 @@ function(win_copy_deps_to_target_dir target)
         if(dep_type STREQUAL "SHARED_LIBRARY")
             if(NOT has_runtime_dll_genex)
                 add_custom_command(TARGET ${target} POST_BUILD
-                    COMMAND ${CMAKE_COMMAND} -P "${SearchEngine_SOURCE_DIR}/cmake/silent_copy.cmake"
+                    COMMAND ${CMAKE_COMMAND} -P "${search-engine_SOURCE_DIR}/cmake/silent_copy.cmake"
                         "$<TARGET_FILE:${dep}>" "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
                     COMMAND_EXPAND_LISTS)
             else()
                 add_custom_command(TARGET ${target} POST_BUILD
-                    COMMAND ${CMAKE_COMMAND} -P "${SearchEngine_SOURCE_DIR}/cmake/silent_copy.cmake"
+                    COMMAND ${CMAKE_COMMAND} -P "${search-engine_SOURCE_DIR}/cmake/silent_copy.cmake"
                         "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
                     COMMAND_EXPAND_LISTS)
             endif()
